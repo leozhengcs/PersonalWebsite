552ab9122bf327dcb926efe3e9551b50c6202232
Leo Zheng <leozhengcs@gmail.com>
2025-06-13 15:01:39 -0700

    Inject Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import NavBar from '$lib/components/NavBar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	import '../app.css';
+	injectAnalytics();
 
 	let { children } = $props();
 </script>
